f7cda4973a57ca5ce507bb732013df18631e5d1c
GrigoriiSolnyshkin <grisolnyshkin04@gmail.com>
2024-04-02 21:45:07 +0300

    added monadic evaluation

--- expr/src/Expr/Eval.hs
+++ expr/src/Expr/Eval.hs
@@ -3,33 +3,52 @@ module Expr.Eval (eval) where
 import Expr.Data
 import qualified Data.Map.Strict as M
 
-eval :: (Floating t, Ord t) => VarList t -> Expr t -> EvalResult t
-eval varList expr = case expr of
-  Lit x -> Right x
-  Var s -> lookupVar s
+import State
+
+evalMonadic :: (Floating t, Ord t) => Expr t -> State (VarList t) (EvalResult t)
+evalMonadic expr = case expr of
+  Lit x -> do return $ Right x
+  Var s -> do
+    state <- get
+    return $ case M.lookup s state of
+      Nothing -> Left (VariableNotFound s)
+      Just value -> Right value
   Add a b -> evalPureBinary (+) a b
   Sub a b -> evalPureBinary (-) a b
   Mul a b -> evalPureBinary (*) a b
-  Div a b -> case (eval' a, eval' b) of
-    (Right _, Right 0.0) -> Left (ZeroInDivision varList b)
-    (Right evalA, Right evalB) -> Right (evalA / evalB)
-    errorEvaluation -> extractError errorEvaluation
-  Pow a b -> case (eval' a, eval' b) of
-    (Right evalA, Right evalB) | evalA <= 0.0 -> Left (NegativeInPower varList a)
-                               | otherwise -> Right (evalA ** evalB)
-    errorEvaluation -> extractError errorEvaluation
-  Sqr a -> case eval' a of
-    Right evalA | evalA < 0.0 -> Left (NegativeInSqrt varList a)
-                | otherwise -> Right (evalA ** 0.5)
-    errorEvaluation -> errorEvaluation
-  where
-    eval' = eval varList
-    extractError :: (EvalResult t, EvalResult t) -> EvalResult t
-    extractError (Left err, _) = Left err
-    extractError (_, Left err) = Left err
-    evalPureBinary op a b = case (eval' a, eval' b) of
-      (Right evalA, Right evalB) -> Right (op evalA evalB)
+  Div a b -> do
+    evalA <- evalMonadic a
+    evalB <- evalMonadic b
+    s <- get
+    return $ case (evalA, evalB) of
+      (Right _, Right 0.0) -> Left (ZeroInDivision s b)
+      (Right resA, Right resB) -> Right $ resA / resB
       errorEvaluation -> extractError errorEvaluation
-    lookupVar s = case M.lookup s varList of
-      Nothing -> Left (VariableNotFound s)
-      Just x -> Right x
+  Pow a b -> do
+    evalA <- evalMonadic a
+    evalB <- evalMonadic b
+    s <- get
+    return $ case (evalA, evalB) of
+      (Right resA, Right _) | resA <= 0 -> Left (NegativeInPower s a)
+      (Right resA, Right resB) -> Right $ resA ** resB
+      errorEvaluation -> extractError errorEvaluation
+  Sqr a -> do
+    eval <- evalMonadic a
+    s <- get
+    return $ case eval of
+      Right res | res < 0 -> Left (NegativeInSqrt s a)
+      Right res -> Right $ res ** 0.5
+      errorEvaluation -> errorEvaluation
+  where
+    evalPureBinary op a b = do
+      evalA <- evalMonadic a
+      evalB <- evalMonadic b
+      return $ case (evalA, evalB) of
+        (Right resA, Right resB) -> Right $ op resA resB
+        errors -> extractError errors
+    extractError (Left error, _) = Left error
+    extractError (_, Left error) = Left error
+
+eval :: (Floating t, Ord t) => VarList t -> Expr t -> EvalResult t
+eval varList expr = execState (evalMonadic expr) varList
+
