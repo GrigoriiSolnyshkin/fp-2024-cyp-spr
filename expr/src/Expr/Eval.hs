08469e5402b6241a8eaae29d2689166313c5f88b
GrigoriiSolnyshkin <grisolnyshkin04@gmail.com>
2024-05-07 23:27:54 +0300

    state -> statet in eval expr

--- expr/src/Expr/Eval.hs
+++ expr/src/Expr/Eval.hs
@@ -1,19 +1,20 @@
 module Expr.Eval (eval, evalMonadic) where
 
-import Control.Monad.State
+import Control.Monad.Trans.State
+import Control.Monad.Trans.Class
 
 import Expr.Data
 import qualified Data.Map.Strict as M
 
 
-evalMonadic :: (Floating t, Ord t) => Expr t -> State (VarList t) (EvalResult t)
+evalMonadic :: (Floating t, Ord t) => Expr t -> StateT (VarList t) (Either (Error t)) t
 evalMonadic expr = case expr of
-  Lit x -> do return $ Right x
+  Lit x -> do return x
   Var s -> do
     state <- get
-    return $ case M.lookup s state of
-      Nothing -> Left (VariableNotFound s)
-      Just value -> Right value
+    case M.lookup s state of
+      Nothing -> lift $ Left (VariableNotFound s)
+      Just value -> return value
   Add a b -> evalPureBinary (+) a b
   Sub a b -> evalPureBinary (-) a b
   Mul a b -> evalPureBinary (*) a b
@@ -21,35 +22,23 @@ evalMonadic expr = case expr of
     evalA <- evalMonadic a
     evalB <- evalMonadic b
     s <- get
-    return $ case (evalA, evalB) of
-      (Right _, Right 0.0) -> Left (ZeroInDivision s b)
-      (Right resA, Right resB) -> Right $ resA / resB
-      errorEvaluation -> extractError errorEvaluation
+    if evalB == 0.0 then lift $ Left (ZeroInDivision s b) else return $ evalA / evalB
   Pow a b -> do
     evalA <- evalMonadic a
     evalB <- evalMonadic b
     s <- get
-    return $ case (evalA, evalB) of
-      (Right resA, Right _) | resA <= 0 -> Left (NegativeInPower s a)
-      (Right resA, Right resB) -> Right $ resA ** resB
-      errorEvaluation -> extractError errorEvaluation
+    if evalA <= 0 then lift $ Left (NegativeInPower s a) else return $ evalA ** evalB
   Sqr a -> do
     eval <- evalMonadic a
     s <- get
-    return $ case eval of
-      Right res | res < 0 -> Left (NegativeInSqrt s a)
-      Right res -> Right $ res ** 0.5
-      errorEvaluation -> errorEvaluation
+    if eval < 0 then lift $ Left (NegativeInSqrt s a) else return $ sqrt eval
   where
+    evalPureBinary :: (Floating t, Ord t) => (t -> t -> t) -> Expr t -> Expr t -> StateT (VarList t) (Either (Error t)) t
     evalPureBinary op a b = do
       evalA <- evalMonadic a
       evalB <- evalMonadic b
-      return $ case (evalA, evalB) of
-        (Right resA, Right resB) -> Right $ op resA resB
-        errors -> extractError errors
-    extractError (Left error, _) = Left error
-    extractError (_, Left error) = Left error
+      return $ op evalA evalB
 
 eval :: (Floating t, Ord t) => VarList t -> Expr t -> EvalResult t
-eval varList expr = evalState (evalMonadic expr) varList
+eval varList expr = evalStateT (evalMonadic expr) varList
 
